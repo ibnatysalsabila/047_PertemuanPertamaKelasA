4f82b69b1137d1b6a40ce29b17f46ff4291baee7
ibnatysalsabila <salsajpr01@gmail.com>
2024-02-27 18:50:14 +0700

    Add project files.

---
+++ PertemuanPertamaKelasA/PertemuanPertamaKelasA/PertemuanPertamaKelasA.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{ //begin
+	float nJejari, nLuas;
+	//numeric njejari, nluas
+	cout << "masukan nJejari = ";
+	//display "Masukan jejari"
+	cin >> nJejari; 
+	//accept "njejari"
+	nLuas = 3.14 * nJejari * nJejari;
+		cout << "luasnya =" << nLuas << endl;
+	
+	
+}
